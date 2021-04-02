e9f8f29fdc801109b4c77735ea90735065294095
Abgar <70165058+MirAbgarAbrahamyan@users.noreply.github.com>
2021-04-02 21:09:25 +0400

    Create Test.java

---
+++ homework1/Test.java
@@ -0,0 +1,17 @@
+package picsartacademy.homework.homework1;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean isOkay = true;
+        byte byteVar = 123;
+        short shortVar = 1222;
+        int intVar = 12343345;
+        long longVar = 12381248476L;
+        float floatVar = 123232323.123F;
+        double doubleVar = 9757320354.343435;
+        char charVar = 'A';
+        System.out.println(isOkay + "\n" + byteVar + "\n" + shortVar + "\n" +
+                           intVar + "\n" + longVar + "\n" + floatVar + "\n" +
+                           doubleVar + "\n" + charVar);
+    }
+}
